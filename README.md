59cf18d3730f9dd57ead84c3adf89cb76c28e4b0
Vladimir Iglovikov <iglovikov@gmail.com>
2023-03-30 14:39:19 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# clip2class_dist
-For clip models for image classification finds class distribution
+# Clip2ClassDist
+
+Clip2ClassDist is a Python script that analyzes image classes by leveraging OpenAI's CLIP model to generate image embeddings. It computes the center and radius of clusters formed by embeddings, the distribution of dot products within each class, and the average, minimum, and maximum dot products between different classes.
+
+The script takes a list of image folders as input, with each folder representing a distinct image class. It uses the specified CLIP model to create normalized embeddings for each image in the folders. The script then analyzes the embeddings using the Faiss library for efficient similarity search and clustering.
+
+## Installation
+
+Clone the repository:
+
+```bash
+git clone https://github.com/ternaus/clip2classdist.git
+```
+
+Install the required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+The main script compute.py can be executed with the following command:
+
+```bash
+Copy code
+python clip2class_dist/compute.py --clip_model MODEL_NAME --image_folders CLASS_NAME_1 FOLDER_PATH_1 CLASS_NAME_2 FOLDER_PATH_2 [--device DEVICE]
+```
+
+Arguments:
+
+* --clip_model: Name of the CLIP model to use (e.g., "ViT-B/32"). Where allowed models:
+  * ViT-B/32
+  * RN50
+  * RN101
+  * RN50x4
+  * RN50x16
+  * ViT-B/16
+  * RN50x64
+  * ViT-L/14
+  * ViT-L/14@336px
+* --image_folders: List of pairs containing class names and corresponding folder paths containing images. Each pair should be separated by a space, and pairs should be space-separated as well (e.g., "dogs /path/to/dog_images cats /path/to/cat_images").
+* --device (optional): Device to run the model on (default: "cpu"). Set to "cuda" if you have a compatible GPU.
+
+Example usage:
+
+```bash
+python clip2class_dist/compute.py --clip_model ViT-B/32 --image_folders dogs /path/to/dog_images cats /path/to/cat_images --device cpu
+```
+
+## Output
+
+The script will output the following information:
+
+1. Cluster centers for each class.
+2. Cluster radii for each class.
+3. Dot product distributions within each class.
+4. Average dot product between different classes.
+5. Minimum dot product between different classes.
+6. Maximum dot product between different classes.
+
+Example output:
+
+```bash
+Cluster centers: {'dogs': array([...]), 'cats': array([...])}
+Cluster radii: {'dogs': 0.2384927, 'cats': 0.27184734}
+Dot product distributions: {'dogs': array([...]), 'cats': array([...])}
+Average dot product between classes: 0.19983745
+Minimum dot product between classes: 0.01759241
+Maximum dot product between classes: 0.38273957
+```
+
+## License
+
+This project is released under the MIT License.
--- clip2classdist/compute.py
+++ clip2classdist/compute.py
@@ -1,20 +1,24 @@
-import os
 import argparse
-from typing import Dict, List, Tuple
-import numpy as np
-import torch
+import os
+
 import clip
 import faiss
+import numpy as np
+import torch
 from PIL import Image
+from tqdm import tqdm
 
 
-def get_embeddings(model: torch.nn.Module, device: str, image_paths: List[str]) -> np.ndarray:
+def get_embeddings(
+    model: torch.nn.Module, preprocess: torch.nn.Sequential, device: str, image_paths: list[str]
+) -> np.ndarray:
     images = [Image.open(path).convert("RGB") for path in image_paths]
     embeddings = []
 
     for image in images:
-        image_tensor = clip.tokenize([image]).to(device)
-        with torch.no_grad():
+        image_tensor = preprocess(image).unsqueeze(0).to(device)
+
+        with torch.inference_mode():
             embedding = model.encode_image(image_tensor)
             embedding = embedding / embedding.norm(dim=-1, keepdim=True)
             embeddings.append(embedding.cpu().numpy())
@@ -22,7 +26,7 @@ def get_embeddings(model: torch.nn.Module, device: str, image_paths: List[str]) 
     return np.concatenate(embeddings, axis=0)
 
 
-def compute_cluster_centers(embeddings: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+def compute_cluster_centers(embeddings: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
     cluster_centers = {}
     for class_name, embed in embeddings.items():
         center = np.mean(embed, axis=0)
@@ -31,56 +35,58 @@ def compute_cluster_centers(embeddings: Dict[str, np.ndarray]) -> Dict[str, np.n
 
 
 def compute_cluster_radii(
-    cluster_centers: Dict[str, np.ndarray], embeddings: Dict[str, np.ndarray]
-) -> Dict[str, float]:
+    cluster_centers: dict[str, np.ndarray], embeddings: dict[str, np.ndarray]
+) -> dict[str, float]:
     cluster_radii = {}
     for class_name, center in cluster_centers.items():
         cluster_radii[class_name] = np.max(np.linalg.norm(embeddings[class_name] - center, axis=1))
     return cluster_radii
 
 
-def compute_dot_product_distributions(embeddings: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+def compute_dot_product_distributions(embeddings: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
     dot_product_distributions = {}
     for class_name, embed in embeddings.items():
         # Create a faiss index
         index = faiss.IndexFlatIP(embed.shape[1])
         index.add(embed)
-        
+
         # Compute the inner product between the embeddings of the same class
         k = embed.shape[0]  # The number of nearest neighbors to search for
-        D, _ = index.search(embed, k)
-        
+        d, _ = index.search(embed, k)
+
         # Set the diagonal elements to zero
-        np.fill_diagonal(D, 0)
-        
+        np.fill_diagonal(d, 0)
+
         # Store the upper triangular part of the dot product matrix
-        dot_product_distributions[class_name] = D[np.triu_indices(D.shape[0], k=1)]
+        dot_product_distributions[class_name] = d[np.triu_indices(d.shape[0], k=1)]
     return dot_product_distributions
 
 
-def compute_cross_class_dot_products(embeddings: Dict[str, np.ndarray]) -> List[float]:
+def compute_cross_class_dot_products(embeddings: dict[str, np.ndarray]) -> list[float]:
     all_dot_products = []
     for class_a, emb_a in embeddings.items():
         for class_b, emb_b in embeddings.items():
+            dot_products = np.dot(emb_a, emb_b.T)
             if class_a == class_b:
-                continue
-            dot_products = faiss.inner_product(emb_a, emb_b)
+                np.fill_diagonal(dot_products, 0)
             all_dot_products.extend(dot_products.flatten().tolist())
     return all_dot_products
 
 
-def main(args: argparse.Namespace):
+def main(args: argparse.Namespace) -> None:
     # Load CLIP model
-    model, _ = clip.load(args.clip_model, device=args.device)
+    model, preprocess = clip.load(args.clip_model, device=args.device)
     model.eval()
 
     # Extract embeddings
     class_embeddings = {}
     for class_name, folder_path in args.image_folders:
         image_paths = [
-            os.path.join(folder_path, img) for img in os.listdir(folder_path) if img.endswith((".jpg", ".jpeg", ".png"))
+            os.path.join(folder_path, img)
+            for img in tqdm(os.listdir(folder_path), desc=f"Processing {folder_path}")
+            if img.endswith((".jpg", ".jpeg", ".png"))
         ]
-        class_embeddings[class_name] = get_embeddings(model, args.device, image_paths)
+        class_embeddings[class_name] = get_embeddings(model, preprocess, args.device, image_paths)
 
     # Compute cluster centers and radii
     cluster_centers = compute_cluster_centers(class_embeddings)
@@ -104,9 +110,21 @@ def main(args: argparse.Namespace):
 
 
 if __name__ == "__main__":
+    allowed_models = [
+        "ViT-B/32",
+        "RN50",
+        "RN101",
+        "RN50x4",
+        "RN50x16",
+        "ViT-B/16",
+        "RN50x64",
+        "ViT-L/14",
+        "ViT-L/14@336px",
+    ]
     parser = argparse.ArgumentParser()
-    parser.add_argument("--clip_model", type=str, required=True, help="Name of the CLIP model.")
+    parser.add_argument("-m", "--clip_model", type=str, required=True, help="Name of the CLIP model.")
     parser.add_argument(
+        "-i",
         "--image_folders",
         nargs="+",
         metavar=("class", "folder"),
@@ -114,7 +132,9 @@ if __name__ == "__main__":
         required=True,
         help="List of class-folder pairs.",
     )
-    parser.add_argument("--device", type=str, default="cpu", help="Device to run the model on (e.g., 'cpu' or 'cuda').")
+    parser.add_argument(
+        "-d", "--device", type=str, default="cpu", help="Device to run the model on (e.g., 'cpu' or 'cuda')."
+    )
     args = parser.parse_args()
 
     main(args)
